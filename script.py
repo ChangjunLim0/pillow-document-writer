5510525f3071b6cb017bd7728a3b0fefe5ce5e39
임창준 Changjun Lim <changjun.lim0@gmail.com>
2025-11-24 05:33:47 +0900

    Chore: Fix hard-coded path problem

--- script.py
+++ script.py
@@ -1,10 +1,14 @@
 import sys
+from pathlib import Path
 
-sys.path.insert(0, "/Users/dolgom/Documents/GitHub/pillow-document-writer/src")
+current_file_path = Path(__file__).resolve()
+project_root = current_file_path.parent
+src_path = str(project_root / "src")
+sys.path.insert(0, src_path)
 
 from pillow_document import DocumentWriter
 
-writer = DocumentWriter(200, 600)
+writer = DocumentWriter(200, 600, margin=40)
 
 writer.write_line("Hello World!", font_size=70)
 writer.write(
