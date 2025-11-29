133ed0822eff8a6f6e1c602e19321aa6de703974
임창준 Changjun Lim <changjun.lim0@gmail.com>
2025-11-30 03:01:18 +0900

    Chore: Update with wrong sentence in script.py

--- script.py
+++ script.py
@@ -8,11 +8,16 @@ sys.path.insert(0, src_path)
 
 from pillow_document import DocumentWriter
 
-writer = DocumentWriter(200, 600, margin=40)
+writer = DocumentWriter(300, 500, margin=40)
 
-writer.write_line("Hello World!", font_size=70)
+writer.write_line("The Fox and the Grapes", font_size=40)
 writer.write(
-    "I am Document Writer. Today is my birthday. Say hello to my potential users"
+    """A Fox one day spied a beautiful bunch of ripe grapes hanging from a vine trained along the branches of a tree. The grapes seemed ready to burst with juice, and the Fox’s mouth watered as he gazed longingly at them.\n
+The bunch hung from a high branch, and the Fox had to jump for it. The first time he jumped he missed it by a long way. So he walked off a short distance and took a running leap at it, only to fall short once more.\n
+Again and again he tried, but in vain. Now he sat down and looked at the grapes in disgust\n
+"What a fool I am," he said. "Here I am wearing myself out to get a bunch of sour grapes that are not worth gaping for."\n
+And off he walked very, very scornfully.""",
+    font_size=18,
 )
 
 writer.save("a.png")
